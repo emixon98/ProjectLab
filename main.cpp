3d19447d046f8791a1dee69a69b0f6f19b80c9ca
Elijah Mixon <emixon98@gmail.com>
2026-05-22 20:59:26 -0700

    Task 3 initial completion

--- main.cpp
+++ main.cpp
@@ -35,7 +35,8 @@ vector<string> bothSports(const vector<Player>& sport1, const vector<Player>& sp
 //Task 2 Function Goal O(N),  0->N
 //Missing only one integer
 int missingInt(const vector<int>& nums){
-    //since start at 0 we can just utilize size 
+    //since start at 0 we can just utilize size since our summation formula is considering starting from index 1
+    //If the difference is 0 then we are simply missing 0 and can just return it
     int n = nums.size();
     //summation formula
     int expectedSum = (n * (n+1)) / 2;
@@ -48,9 +49,21 @@ int missingInt(const vector<int>& nums){
 }
 
 //Task 3 Function Goal O(N)
-//int greatestProfit(const vector<int>& stockPredictions){
+int greatestProfit(const vector<int>& stockPredictions){
+    //starting vals/placeholders
+    int low = stockPredictions[0];
+    int profit = 0;
 
-// }
+    for (int i = 1; i < stockPredictions.size(); i++){
+        //Var to hold the difference between our buy and sell point, if our potential is lower than our current calculated profit
+        // then we dont change our curr profit margin
+        int potential = stockPredictions[i] - low;
+        //can use max and min function to find our lowest buy point and highest sell point by comparing through iteration over n
+        profit = max(profit, potential);
+        low = min(low, stockPredictions[i]);
+    }
+    return profit;
+}
 
 //Task 4 Goal O(N)
 //int highestProduct(const vector<int>& vals){
@@ -72,7 +85,13 @@ int missingInt(const vector<int>& nums){
 int main(){
     vector<int> nums = {2, 3, 4, 5, 6, 0, 1};
 
-    cout << missingInt(nums);
+    cout << missingInt(nums) << endl;
+
+    //write test for task1, kind of dont want to atm, could copy paste from lab just ensuring I implement struct datatype
+
+    vector<int> stocks = {10, 7, 5, 8, 11, 2, 6};
+    //correct return of 6 can add $ for formatting, but test niche cases and other options as well
+    cout << greatestProfit(stocks) << endl;
 
     return 0;
 }
